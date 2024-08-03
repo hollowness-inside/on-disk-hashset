945688a6ab66df1dcb146f48d5f5ce2fd5a9715d
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-08-03 15:37:06 +1000

    Update example

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import fhs
 
-f = fhs.convert(['cat', 1, '2', 3, 'dog'], 'hash.set')
-
-print('cat',   f.has('cat'))
-print('dog',   f.has('dog'))
-print('snake', f.has('snake'))
+with fhs.convert(['cat', 1, '2', 3, 'dog'], 'hash.set') as f:
+    print('cat',   f.has('cat'))
+    print('dog',   f.has('dog'))
+    print('snake', f.has('snake'))
