afe2d7ce9d41c49cbf020aee235e12bdb6fdcac3
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-08-03 15:28:07 +1000

    Create `convert` function

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ def HASH(x: any) -> int:
 
     digest = hashlib.sha256(x).digest()
     return int.from_bytes(digest, byteorder='big')
+
+
+class FileHashSet:
+    pass
+
+
+def convert(array: list[any], fout: str) -> FileHashSet:
+    modulo = len(array)
+    hashmap = [(i, set()) for i in range(modulo)]
+
+    for item in array:
+        id = HASH(item) % modulo
+        hashmap[id][1].add(item)
+
+    print(hashmap)
+
+
+convert(['cat', 1, '2', 3, 'dog'], 'hash.set')
